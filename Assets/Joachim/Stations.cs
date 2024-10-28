250e4ab26acadcae2b3a27bdffc0f38ee1e25dc6
JoachimLaine <joachim.laine37@gmail.com>
2024-10-28 23:29:09 +0100

    :+1:

--- Assets/Joachim/Stations.cs
+++ Assets/Joachim/Stations.cs
@@ -4,29 +4,44 @@ using UnityEngine;
 
 public class Stations : MonoBehaviour
 {
-    public GameObject sewingStation;
-    public GameObject paintingStation;
-    public GameObject assemblyStation;
-    public GameObject electricStation;
-    public GameObject Carpentry;
-    public GameObject Forge;
-    public GameObject suppliesZone;
 
-    // Update is called once per frame
+    [SerializeField] private Transform 
+
+    void Start()
+    {
+        //déclarer 
+    }
+
     void Update()
     {
-
+        //ramasser un objet
         void Pickup()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Physics.Raycast(transform.position);
+            }
+        }
+
+        void Drop()
         {
 
         }
 
+        //interagir avec la station si les conditions requises sont complétées
         void InteractionStation()
         {
 
         }
 
+        //interagir avec la station pour la remplir
         void RefillStation()
+        {
+
+        }
+
+        //pour commander les ravitaillements et des pizzas
+        void Calzone()
         {
 
         }

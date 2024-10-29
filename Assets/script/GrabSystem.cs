a8b4efa452244487efc80e30523b1877825937c8
Preonzz <tomempis05@gmail.com>
2024-10-29 16:51:39 +0100

    Avancement grab system

--- Assets/script/GrabSystem.cs
+++ Assets/script/GrabSystem.cs
@@ -1,18 +1,27 @@
+using Mirror.Examples.MultipleMatch;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GrabSystem : MonoBehaviour
 {
+    InputAction interactAction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        interactAction = InputSystem.actions.FindAction("Interact");
     }
-
-    IEnumerator GrabAction()
+    void Update()
     {
-        
-        yield return null;
+        GrabAction();
+    }
+    void GrabAction()
+    {
+        if (interactAction.IsPressed())
+        {
+            Debug.Log("Boutton Pressé");
+        }
     }
 }

cfb9a4d38f44997c5ecfc8bfa5ac4d8ea88aa02d
Philia's Snake <snakephilias@gmail.com>
2024-10-29 16:17:33 +0100

    supression et modification des scripts

--- Assets/Dossier_du_jeu/Scripts_C#/Scripts-UI/MenuManager.cs
+++ Assets/Dossier_du_jeu/Scripts_C#/Scripts-UI/MenuManager.cs
@@ -1,8 +1,28 @@
+using Mirror.BouncyCastle.Crypto.Generators;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
 
+    public void OkButton()
+    {
+        Application.Quit();
+    }
+
+    public void OpenMenu(GameObject open)
+    {
+        open.SetActive(true);
+    }
+
+    public void CloseMenu(GameObject close)
+    {
+        close.SetActive(true);
+    }
 }

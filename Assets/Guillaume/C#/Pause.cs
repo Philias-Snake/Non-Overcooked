68ca8094c3e9eba7c6140e7dcb48d25e05b9d6c1
Philia's Snake <snakephilias@gmail.com>
2024-10-29 09:41:31 +0100

    Update Pause.cs

--- Assets/Guillaume/C#/Pause.cs
+++ Assets/Guillaume/C#/Pause.cs
@@ -1,10 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
+    public GameObject pauseUI;
 
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (pauseUI.activeSelf)
+            {
+                ClosePause();
+            }
+            else
+            {
+                OpenPause();
+            }
+        }
+    }
+
+    public void OpenPause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ClosePause()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1;
+    }
 
     public void OpenMenu(GameObject open)
     {

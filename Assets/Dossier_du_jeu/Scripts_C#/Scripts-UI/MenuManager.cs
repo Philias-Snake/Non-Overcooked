a95fd151c5e49125f837ae03e24c0bceb919dabc
Philia's Snake <snakephilias@gmail.com>
2024-10-30 14:00:16 +0100

    Fabriction de scripts pour les menus et les textures
    J'ai créer un script permettant de corriger les erreurs des cubes map créer par le system de miror et du local

--- Assets/Dossier_du_jeu/Scripts_C#/Scripts-UI/MenuManager.cs
+++ Assets/Dossier_du_jeu/Scripts_C#/Scripts-UI/MenuManager.cs
@@ -1,8 +1,22 @@
 using UnityEngine;
-public static class MenuManager
+
+public class MenuManager : MonoBehaviour
 {
-    public static bool IsInitialised { get; private set; }
+    public static MenuManager Instance { get; private set; }
     public static GameObject menuMenu, soundMenu, quitMenu;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     public static void Init()
     {
@@ -10,14 +24,11 @@ public static class MenuManager
         menuMenu = canvas.transform.Find("MenuMenu").gameObject;
         soundMenu = canvas.transform.Find("SoundMenu").gameObject;
         quitMenu = canvas.transform.Find("QuitMenu").gameObject;
-
-        IsInitialised = true;
     }
-
 
     public static void OpenMenu(Menu menu, GameObject callingMenu)
     {
-        if (!IsInitialised)
+        if (!menuMenu || !soundMenu || !quitMenu)
             Init();
 
         switch (menu)

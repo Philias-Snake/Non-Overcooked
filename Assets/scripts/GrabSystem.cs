628123e358cc084f546090cfdae00dfd0f9e62ba
Preonzz <tomempis05@gmail.com>
2024-12-03 10:06:57 +0100

    Suite GrabSystem

--- Assets/scripts/GrabSystem.cs
+++ Assets/scripts/GrabSystem.cs
@@ -3,15 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class GrabSystem : MonoBehaviour
 {
     InputAction interactAction;
+    public Transform grabPosition;
+
+    private bool grabing;
 
     // Start is called before the first frame update
     void Start()
     {
         interactAction = InputSystem.actions.FindAction("Interact");
+        grabing = false;
     }
     void Update()
     {
@@ -22,6 +27,14 @@ public class GrabSystem : MonoBehaviour
         if (interactAction.IsPressed())
         {
             Debug.Log("Boutton Pressé");
+            grabing = true;
+        }
+
+        while (grabing = true)
+        {
+
         }
     }
+
+    
 }

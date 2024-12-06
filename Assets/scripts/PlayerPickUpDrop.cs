12348001241a567b7442b19472ae32b4df90c5af
JoachimLaine <joachim.laine37@gmail.com>
2024-12-06 10:09:34 +0100

    ptn ça marche les gars

--- Assets/scripts/PlayerPickUpDrop.cs
+++ Assets/scripts/PlayerPickUpDrop.cs
@@ -9,8 +9,13 @@ public class PlayerPickUpDrop : MonoBehaviour
     [SerializeField] Transform raycastOriginTransform;
     [SerializeField] Transform objectGrabPositionTransform;
     [SerializeField] LayerMask pickUpLayerMask;
+
     private GrabbableObject grabbableObject;
     public float pickupDistance = 2.0f;
+
+    private GameObject grabbed = null;
+    private GameObject HoldObj;
+    public float action = 3.5f;
 
     private void Awake()
     {
@@ -26,8 +31,11 @@ public class PlayerPickUpDrop : MonoBehaviour
                 if (Physics.Raycast(raycastOriginTransform.position, raycastOriginTransform.forward, out RaycastHit raycastHit,
                     pickupDistance, pickUpLayerMask)) {
 
-                    if (raycastHit.transform.TryGetComponent(out grabbableObject)) {
+                    if (raycastHit.transform.TryGetComponent(out grabbableObject)) 
+                    {
+                        HoldObj = raycastHit.transform.gameObject;
                         grabbableObject.Grab(objectGrabPositionTransform);
+                        grabbed = null;
                     }
                 }
             } 
@@ -35,6 +43,133 @@ public class PlayerPickUpDrop : MonoBehaviour
                 //Transport d'un objet en cours 
                 grabbableObject.Drop();
                 grabbableObject = null;
+                grabbed = HoldObj;
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "RedPaint Station" && grabbed.tag == "Unpainted Toy") // && bool objet tenu + bool objet non paint
+        {
+            Debug.Log("Station de peinture");
+            if (Input.GetKeyDown("E"))
+            {
+                //animation
+                if (Input.GetKeyDown("E") && Time.time > action)
+                {
+                    grabbed.tag = "Painted Toy";
+                    //sprite de l'objet change
+                }
+            }
+        }
+
+        if (other.tag == "BluePaint Station" && grabbed.tag == "Unpainted Toy") // && bool objet tenu + bool objet non paint
+        {
+            Debug.Log("Station de peinture");
+            if (Input.GetKeyDown("E"))
+            {
+                //animation
+                if (Input.GetKeyDown("E") && Time.time > action)
+                {
+                    grabbed.tag = "Painted Toy";
+                    //sprite de l'objet change
+                }
+            }
+        }
+
+        if (other.tag == "GreenPaint Station" && grabbed.tag == "Unpainted Toy") // && bool objet tenu + bool objet non paint
+        {
+            Debug.Log("Station de peinture");
+            if (Input.GetKeyDown("E"))
+            {
+                //animation
+                if (Input.GetKeyDown("E") && Time.time > action)
+                {
+                    grabbed.tag = "Painted Toy";
+                    //sprite de l'objet change
+                }
+            }
+        }
+
+        if (other.tag == "Packaging Station" && grabbed.tag == "Painted Toy")
+        {
+            Debug.Log("Station d'emballage");
+            if (Input.GetKeyDown("E"))
+            {
+                //animation
+                if (Input.GetKeyDown("E") && Time.time > action)
+                {
+                    grabbed.tag = "Gift";
+                    //sprite de l'objet change
+                }
+            }
+        }
+
+        if (other.tag == "Sleigh" && grabbed.tag == "Gift")
+        {
+            Debug.Log("Traîneaux export");
+            if (Input.GetKeyDown("E"))
+            {
+                Destroy(grabbed);
+                //Score système
+            }
+        }
+
+        if (other.tag == "Sewing Station")
+        {
+            Debug.Log("Station Couture");
+            if (Input.GetKeyDown("E"))
+            {
+                //animation
+                if (Input.GetKeyDown("E") && Time.time > action)
+                {
+                    grabbed.tag = "Painted Toy";
+                    //sprite de l'objet change
+                }
+            }
+        }
+
+        if (other.tag == "Forge" && grabbed.tag == "Iron")
+        {
+            Debug.Log("Forge");
+            if (Input.GetKeyDown("E"))
+            {
+                //animation
+                if (Input.GetKeyDown("E") && Time.time > action)
+                {
+                    grabbed.tag = "Painted Toy";
+                    //sprite de l'objet change
+                }
+            }
+        }
+
+        if (other.tag == "Carpentry" && grabbed.tag == "Wood")
+        {
+            Debug.Log("Menuiserie");
+            if (Input.GetKeyDown("E"))
+            {
+                //animation
+                if (Input.GetKeyDown("E") && Time.time > action)
+                {
+                    grabbed.tag = "Painted Toy";
+                    //sprite de l'objet change
+                }
+            }
+        }
+
+        if (other.tag == "Electronic Station" && grabbed.tag == "Eletric Toy")
+        {
+            Debug.Log("Station jouets éléctriques");
+            if (Input.GetKeyDown("E"))
+            {
+                //animation
+                if (Input.GetKeyDown("E") && Time.time > action)
+                {
+                    grabbed.tag = "Unpainted Toy";
+                    //sprite de l'objet change
+                }
             }
         }
     }
